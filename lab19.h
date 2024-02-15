d74d979503e321987d24d3065af7cfb1b5761273
nani <chotima.nani@gmail.com>
2024-02-15 15:29:00 +0700

    completed

--- lab19.h
+++ lab19.h
@@ -63,13 +63,31 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+	return hp <= 0;
+}
 
+void Unit::guard(){
+	guard_on = true;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::beAttacked(int oppatk){
+	int damage = oppatk - def;
+	if(guard_on) damage /= 3;
+	hp -= damage;
+	return damage;
+}
 
+int Unit::attack(Unit & target){
+	return target.beAttacked(atk);
+}
 
+int Unit::heal(){
+	int heal = rand() % 21+10;
+	if(hp + heal > hpmax)  heal = hpmax - hp;
+	hp += heal;
+	return heal;
+}
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
